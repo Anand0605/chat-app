a984a81e2e97788a142e76d600cc51782f2d0874
Anand Kumar Gautam <anandvns0510@gmail.com>
2024-07-08 03:45:52 +0530

    complete crud app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,69 @@
 import 'remixicon/fonts/remixicon.css'
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
-const App = () => {
-  const [right, setRight] = useState(-450)
+const model ={
+  fullname: '',
+    class: '',
+    subject: '',
+    roll: '',
+    dob: '',
+}
 
-  const handleDrawer =()=>{
+const App = () => {
+  const[editIndex,setEditIndex]=useState(null)
+  const [right, setRight] = useState(-450)
+  const [students, setStudents] = useState([])
+  const [form, setForm] = useState(model)
+
+  const handleDrawer = () => {
     setRight(0)
+  }
+
+  const handleInput = (e) => {
+    const input = e.target
+    const value = input.value
+    const key = input.name
+    setForm({
+      ...form,
+      [key]: value
+    })
+  }
+
+  const createStudent = (e) => {
+    e.preventDefault()
+    setStudents([...students, form])
+    setForm(model)
+    setRight(-450)
+  }
+
+
+  const deleteStudent=(index)=>{
+    const backup = [...students]
+    backup.splice(index,1)
+    setStudents(backup)
+  }
+
+  const editStudent=(index)=>{
+    setRight(0)
+    setForm(students[index])
+    setEditIndex(index)
+  }
+
+  const saveStudent=(e)=>{
+    e.preventDefault()
+    const backups = [...students]
+    backups[editIndex]=form
+    setStudents(backups)
+    setForm(model)
+    setEditIndex(null)
+    setRight(-450)
+  }
+
+  const closeDrawer=()=>{
+   setRight(-450)
+   setForm(model)
+   setEditIndex(null)
   }
   return (
     <div style={{
@@ -24,122 +80,73 @@ const App = () => {
           padding: "0",
           margin: "0",
           textAlign: "center"
-        }}> React Crud App</h1>
+        }}>React Crud App</h1>
         <button
-        onClick={handleDrawer}
-         style={{
-          border: "none",
-          background: "#8407ba",
-          color: "white",
-          padding: "14px 22px",
-          borderRadius: "4px",
-          fontSize: "16px",
-          margin:"16px"
-        }}>
+          onClick={handleDrawer}
+          style={{
+            border: "none",
+            background: "#8407ba",
+            color: "white",
+            padding: "14px 22px",
+            borderRadius: "4px",
+            fontSize: "16px",
+            margin: "16px"
+          }}>
           <i className="ri-user-add-line" style={{ marginRight: "5px" }}></i>
-          New Student</button>
-          <table className='crud-app'>
-            <thead>
-              <tr>
-                <th>S/No</th>
-                <th>Student's name</th>
-                <th>Subject</th>
-                <th>Class</th>
-                <th>Roll.No</th>
-                <th>DOB</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>Er.Niket</td>
-                <td>React</td>
-                <td>19133</td>
-                <td>B.tech(cse)</td>
-                <td>27/11/2002</td>
-                <td>
-                  <div>
-                    <button style={{
-                      height:"32px",
-                      width:"32px",
-                      borderRadius:"4px",
-                      border:"none",
-                      background:"green",
-                      color:"white",
-                      marginRight:12
-
-                    }}><i className="ri-image-edit-fill"></i></button>
-                    <button style={{
-                      height:"32px",
-                      width:"32px",
-                      borderRadius:"4px",
-                      border:"none",
-                      background:"red",
-                      color:"white"
-
-                    }}><i class="ri-chat-delete-line"></i></button>
-                  </div>
-                </td>
-              </tr>
-              {/* <tr>
-                <td>2</td>
-                <td>Er.Niket</td>
-                <td>React</td>
-                <td>19133</td>
-                <td>B.tech(cse)</td>
-                <td>27/11/2002</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td>Er.Niket</td>
-                <td>React</td>
-                <td>19133</td>
-                <td>B.tech(cse)</td>
-                <td>27/11/2002</td>
-              </tr>
-              <tr>
-                <td>4</td>
-                <td>Er.Niket</td>
-                <td>React</td>
-                <td>19133</td>
-                <td>B.tech(cse)</td>
-                <td>27/11/2002</td>
-              </tr>
-              <tr>
-                <td>5</td>
-                <td>Er.Niket</td>
-                <td>React</td>
-                <td>19133</td>
-                <td>B.tech(cse)</td>
-                <td>27/11/2002</td>
-              </tr>
-              <tr>
-                <td>6</td>
-                <td>Er.Niket</td>
-                <td>React</td>
-                <td>19133</td>
-                <td>B.tech(cse)</td>
-                <td>27/11/2002</td>
-              </tr>
-              <tr>
-                <td>7</td>
-                <td>Er.Niket</td>
-                <td>React</td>
-                <td>19133</td>
-                <td>B.tech(cse)</td>
-                <td>27/11/2002</td>
-              </tr>
-              <tr>
-                <td>8</td>
-                <td>Er.Niket</td>
-                <td>React</td>
-                <td>19133</td>
-                <td>B.tech(cse)</td>
-                <td>27/11/2002</td>
-              </tr> */}
-              </tbody>
-          </table>
+          New Student
+        </button>
+        <table className='crud-app'>
+          <thead>
+            <tr>
+              <th>S/No</th>
+              <th>Student's name</th>
+              <th>Subject</th>
+              <th>Class</th>
+              <th>Roll.No</th>
+              <th>DOB</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              students.map((value, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{value.fullname}</td>
+                  <td>{value.subject}</td>
+                  <td>{value.class}</td>
+                  <td>{value.roll}</td>
+                  <td>{value.dob}</td>
+                  <td>
+                    <div>
+                      <button
+                      onClick={()=>editStudent(index)} 
+                      style={{
+                        height: "32px",
+                        width: "32px",
+                        borderRadius: "4px",
+                        border: "none",
+                        background: "green",
+                        color: "white",
+                        marginRight: 12
+                      }}><i className="ri-image-edit-fill"></i></button>
+                      <button 
+                      onClick={()=>deleteStudent(index)}
+                      style={{
+                        height: "32px",
+                        width: "32px",
+                        borderRadius: "4px",
+                        border: "none",
+                        background: "red",
+                        color: "white"
+                      }}><i className="ri-chat-delete-line"></i></button>
+                    </div>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
       </div>
       <aside style={{
         position: "fixed",
@@ -151,63 +158,99 @@ const App = () => {
         padding: 32,
         boxShadow: '0 0 40px rgba(0,0,0,0.2)',
         boxSizing: "border-box",
-        transition:"0.2"
+        transition: "0.2s"
       }}>
-        <button 
-        onClick={()=>setRight(-450)}
-        style={{border:"none",
-          fontSize:"20px",
-          background:"white",
-          position:"absolute",
-          top:"20px",
-          right:"20px"
+        <button
+          onClick={closeDrawer}
+          style={{
+            border: "none",
+            fontSize: "20px",
+            background: "white",
+            position: "absolute",
+            top: "20px",
+            right: "20px"
           }}><i className="ri-close-circle-line"></i></button>
         <h1>New Student</h1>
-        <form style={{
-          display:"flex",
-          flexDirection:"column",
-          gap:"30px"}}>
+        <form
+          onSubmit={ editIndex === null ? createStudent :saveStudent}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "30px"
+          }}>
           <input
-          required
-          name='Fullname'
-           type="text"
-           placeholder='Enter your fullname..'
-           style={{border:"1px solid #ccc",
-            padding:16,}} />
+          spellCheck='true'
+            value={form.fullname}
+            onChange={handleInput}
+            required
+            name='fullname'
+            type="text"
+            placeholder='Enter your fullname..'
+            style={{
+              border: "1px solid #ccc",
+              padding: 16,
+            }} />
           <input
-          required
-          name='Classname'
-           type="Number"
-           placeholder='Enter your Classname..'
-           style={{border:"1px solid #ccc",
-            padding:16,}} />
+            value={form.class}
+            onChange={handleInput}
+            required
+            name='class'
+            type="number"
+            placeholder='Enter your class..'
+            style={{
+              border: "1px solid #ccc",
+              padding: 16,
+            }} />
           <input
-          required
-          name='Roll.No'
-           type="Number"
-           placeholder='Enter your Roll.No..'
-           style={{border:"1px solid #ccc",
-            padding:16,}} />
+            value={form.roll}
+            onChange={handleInput}
+            required
+            name='roll'
+            type="number"
+            placeholder='Enter your roll no..'
+            style={{
+              border: "1px solid #ccc",
+              padding: 16,
+            }} />
           <input
-          required
-          name='Subject'
-           type="Number"
-           placeholder='Enter your Subject..'
-           style={{border:"1px solid #ccc",
-            padding:16,}} />
+            value={form.subject}
+            onChange={handleInput}
+            required
+            name='subject'
+            type="text"
+            placeholder='Enter your subject..'
+            style={{
+              border: "1px solid #ccc",
+              padding: 16,
+            }} />
           <input
-          required
-          name='DOB'
-           type="date"
-           placeholder='Enter your date..'
-           style={{border:"1px solid #ccc",
-            padding:16,}} />
-            <button style={{
-              border:"none",
-              background:"#8407ba",
-              color:"white",
-              fontSize:"16px",
-              padding:"14px 25px"}}>SUBMIT</button>
+            value={form.dob}
+            onChange={handleInput}
+            required
+            name='dob'
+            type="date"
+            style={{
+              border: "1px solid #ccc",
+              padding: 16,
+            }} />
+            {
+              editIndex===null ? <button style={{
+                border: "none",
+                background: "#8407ba",
+                color: "white",
+                fontSize: "16px",
+                padding: "14px 25px"
+              }}>SUBMIT</button>
+              : <button style={{
+                border: "none",
+                background: "deeppink",
+                color: "white",
+                fontSize: "16px",
+                padding: "14px 25px"
+              }}>SAVE</button>
+            }
+         
+         
         </form>
       </aside>
     </div>
